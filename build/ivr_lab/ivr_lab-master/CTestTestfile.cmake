e51ee148e5baaa2f8b04b8dcbdc92302ca74d555
Your Name <604879808@qq.com>
2019-10-09 17:08:21 +0100

    First commit

---
+++ build/ivr_lab/ivr_lab-master/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dice/catkin_ws/src/ivr_lab/ivr_lab-master
+# Build directory: /home/dice/catkin_ws/build/ivr_lab/ivr_lab-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
